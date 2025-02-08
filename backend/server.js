8afeb2c885cc3602f249a97d70da35b0f27f8e05
Shivamsingh116-hub <shivamsinghk116@gmail.com>
2025-02-08 14:15:02 +0530

    update some

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const userRouter = require("./routes/userRoutes")
 const doctorRouter = require("./routes/doctorRoutes")
 const pharmacistRoute = require("./routes/pharmacistRoute")
 const app = express()
+const http = require('http')
+const { Server } = require('socket.io')
+const server = http.createServer(app)
 app.use(express.json())
 require("dotenv").config()
 app.use(CORS())
@@ -14,6 +17,34 @@ app.use(pharmacistRoute)
 app.get('/', (req, res) => {
     res.send("WORKING")
 })
-app.listen(process.env.PORT, () => {
+const io = new Server(server, {
+    cors: { origin: "*" }
+})
+const users = {}
+io.on("connection", (socket) => {
+    console.log("user id", socket.id);
+    socket.on("register", (phoneNumber) => {
+        users[phoneNumber] = socket.id
+    })
+    socket.on("sendMessage", ({ to, message }) => {
+        console.log(users)
+        let recieverSocketId = users[to]
+        if (recieverSocketId) {
+            io.to(recieverSocketId).emit('receiveMessage', message)
+        } else {
+            socket.emit("error", "Currently Doctor not available...")
+        }
+    })
+    socket.on("disconnect", () => {
+        for (const phone in users) {
+            if (users[phone] === socket.id) {
+                delete users[phone]
+                break;
+            }
+        }
+        console.log("User disconnected:", socket.id);
+    })
+})
+server.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`)
 })
--- frontend/src/component/contact/Contact.jsx
+++ frontend/src/component/contact/Contact.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import { io } from "socket.io-client";
+import { Context } from '../../common/Context';
+const socket = io("http://localhost:3000")
 const Contact = () => {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const [senderNumber, setSenderNumber] = useState('')
+  const { currentUser } = useContext(Context)
+  const contact = currentUser.contact
+  useEffect(() => {
+    socket.on('receiveMessage', (msg) => {
+      setMessages((prevMsg) => [...prevMsg, `Other: ${msg}`])
+    })
+    socket.on("error", (err) => {
+      alert(err);
+    });
+    return () => {
+      socket.off('receiveMessage')
+    }
+  }, [])
+  const sendMessage = () => {
+    socket.emit('register', contact)
+    socket.emit('sendMessage', { to: senderNumber, message })
+    setMessages((prevMsg) => [...prevMsg, `Me: ${message}`])
+    setMessage('')
+  }
+
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Chat</h2>
+      <ul>
+        {
+          messages && messages.map((msg) => {
+            return <li>{msg}</li>
+          })
+        }
+      </ul>
+      <div>
+        <label>Send to </label>
+        <input type='number' value={senderNumber} onChange={(e) => setSenderNumber(e.target.value)} placeholder='Enter number her ' />
+        <input value={message} onChange={(e) => setMessage(e.target.value)} id='message' placeholder='enter message' />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
   )
 }
 
