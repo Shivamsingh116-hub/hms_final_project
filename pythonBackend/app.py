070333f7acfe5b144301f718fb9e5061ed6f1c93
Shivamsingh116-hub <shivamsinghk116@gmail.com>
2025-02-09 16:07:32 +0530

    update pytho backend

--- pythonBackend/app.py
+++ pythonBackend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask,jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
+import os
 from controller.billing2route import  medicine_data
 app=Flask(__name__)
-CORS(app)
+CORS(app,origins=[os.getenv('LOCALHOST_URL'),os.getenv('FRONTEND_URL')])
 app.register_blueprint(medicine_data,url_prefix='/billing2data')
 @app.route('/')
 def hello():
